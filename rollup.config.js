9bd80d91685ffe894f9a949ebd22768091244c49
Hajime Yamasaki Vukelic <hayavuk@gmail.com>
2019-07-14 09:16:12 +0200

    Do not provide UMD modules (usage too confusing)

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,12 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
 import pkg from './package.json';
 
 export default [
-	{
-		input: 'src/index.js',
-		output: {
-			name: 'transplexer-tools',
-			file: pkg.browser,
-			format: 'umd',
-		},
-		plugins: [
-			resolve(),
-			commonjs(),
-		],
-	},
 	{
 		input: 'src/index.js',
 		output: [
 			{ file: pkg.main, format: 'cjs' },
 			{ file: pkg.module, format: 'es' }
 		],
+    external: ['transplexer'],
 	},
 ];
